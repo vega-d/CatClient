fab8e91f02de7337b9ff59e672301b065d443196
Veseha <sveula@list.ru>
2020-05-12 14:51:05 +0600

    PyQT - fix

--- manager.py
+++ manager.py
@@ -10,47 +10,6 @@ from data import db_session
 from data.users import User
 
 
-class Main(QWidget):
-    def __init__(self):
-        super().__init__()
-        # self.setWindowFlags(Qt.Window)
-        if __name__ == '__main__':
-            uic.loadUi(resource_path('qt\\main_qt.ui'), self)
-        else:
-            uic.loadUi(resource_path('qt\\main_qt.ui'), self)
-        self.initUI()
-
-    def initUI(self):
-        self.setWindowTitle('Admin panel')
-        # ----------------- List_user -----------------
-        db_session.global_init("db/catclient.sqlite")
-        session = db_session.create_session()
-        users = session.query(User).all()
-        del users[0]
-        list_name_user = [i.name for i in users]
-        self.list_user.addItems(list_name_user)
-        self.bselect_user.clicked.connect(self.select_user)
-        # ----------------- List_dirs -----------------
-        self.bselect_dir.clicked.connect(self.select_dir)
-
-    def select_user(self):
-        user = self.list_user.currentText()
-        session = db_session.create_session()
-        users = session.query(User).filter(User.name == user).first()
-        if users:
-            print(users.dirs)
-            if users.dirs is None:
-                self.list_dirs.addItems('Not found')
-            else:
-                print(users.dirs)
-                dirs = users.dirs.split(',')
-                self.list_dirs.addItems(dirs)
-
-
-    def select_dir(self):
-        None
-
-
 def server():
     print("Starting up the server")
     try:
@@ -67,18 +26,19 @@ def server():
 class App(QWidget):
     def __init__(self):
         super().__init__()
-        self.main_app = Main()
+        if __name__ == '__main__':
+            uic.loadUi(resource_path('qt\\main_qt.ui'), self)
+        else:
+            uic.loadUi(resource_path('qt\\main_qt.ui'), self)
+
         self.move(180, 100)
-        self.setFixedSize(400, 300)
+        self.setFixedSize(480, 480)
         self.setWindowTitle('control')
 
         self.server = threading.Thread(target=server)
 
         self.toclose = False
         self.all_buttons = []
-
-        self.start_button = QPushButton('start/stop', parent=self)
-        self.start_button.setGeometry(100, 100, 200, 100)
         self.start_button.action = 'start'
         self.all_buttons.append(self.start_button)
 
@@ -98,17 +58,45 @@ class App(QWidget):
         self.tray_icon.setIcon(self.style().standardIcon(QStyle.SP_ComputerIcon))
         self.tray_icon.setContextMenu(tray_menu)
         self.tray_icon.show()
+        # ----------------- List_user -----------------
+        db_session.global_init("db/catclient.sqlite")
+        session = db_session.create_session()
+        users = session.query(User).all()
+        del users[0]
+        list_name_user = [i.name for i in users]
+        self.list_user.addItems(list_name_user)
+        self.bselect_user.clicked.connect(self.select_user)
+        # ----------------- List_dirs -----------------
+        self.bselect_dir.clicked.connect(self.select_dir)
+
+    def select_user(self):
+        user = self.list_user.currentText()
+        session = db_session.create_session()
+        users = session.query(User).filter(User.name == user).first()
+        if users:
+            print(users.dirs)
+            if users.dirs is None:
+                self.list_dirs.clear()
+                self.list_dirs.addItems(['Not found'])
+
+            else:
+                self.list_dirs.clear()
+                dirs = users.dirs.split(',')
+                self.list_dirs.addItems(dirs)
+
+    def select_dir(self):
+        None
 
     def onClick(self):
         btn = self.sender().action
         print(btn, 'action released!')
-        if btn == 'start':
-            if not self.server.is_alive():
-                self.main_app.show()
-
-                self.server.start()
-                self.start_button.setEnabled(0)
-                self.start_button.setText('server running')
+        # if btn == 'start':
+        #     if not self.server.is_alive():
+        #         self.main_app.show()
+        #
+        #         self.server.start()
+        #         self.start_button.setEnabled(0)
+        #         self.start_button.setText('server running')
 
     def closeEvent(self, evnt):
         if self.toclose:
